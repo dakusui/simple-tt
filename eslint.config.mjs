0061382e15dc914d23b1212bb6e8f19e495affdb
Hiroshi Ukai <dakusui@gmail.com>
2025-02-04 04:23:11 +0900

    Add prettier settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from "prettier";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "dot-location": ["error", "property"], // Ensure dots stay with properties
+      "no-extra-parens": "off" // Avoid conflicts with Prettier
+    }
+  }
 ];
