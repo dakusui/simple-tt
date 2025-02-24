4e02a662365148a7c368155e9a7ec1e4e267ae7b
Hiroshi Ukai <dakusui@gmail.com>
2025-02-24 13:56:37 +0900

    feat: make test case history page flexible

--- src/app/test-case-history/[testSuiteId]/[testCaseId]/page.tsx
+++ src/app/test-case-history/[testSuiteId]/[testCaseId]/page.tsx
@@ -1,12 +1,22 @@
-import TestCaseRunHistory from "@/components/TestCaseRunHistory";
+'use client'
 
-export default async function TestCaseRunsPage({
-  params,
-}: {
-  params: { testSuiteId: string; testCaseId: string };
-}) {
-  const resolvedParams = await params;
+import TestCaseRunHistory from "@/components/TestCaseRunHistory";
+import { TestCaseRunWithTriage } from "@/models/test-entities";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useParams, useRouter } from "next/navigation";
+
+export default function TestCaseRunsPage() {
+  const router: AppRouterInstance = useRouter();
+  const resolvedParams: {testSuiteId:string, testCaseId: string} = useParams();
+  const selection = (runId: string, testCase: TestCaseRunWithTriage) => {
+    console.log("Selected test case", testCase);
+    router.push(`/triage/${encodeURIComponent(runId)}/${encodeURIComponent(testCase.testSuiteId)}/${encodeURIComponent(testCase.testCaseId)}`);
+  };
   return (
-    <TestCaseRunHistory testSuiteId={resolvedParams.testSuiteId}  testCaseId={resolvedParams.testCaseId} />
+    <TestCaseRunHistory
+      testSuiteId={resolvedParams.testSuiteId}
+      testCaseId={resolvedParams.testCaseId}
+      onSelect={selection}
+    />
   );
 }
