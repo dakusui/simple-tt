d31cb555a59ee378b109d0efa215f5426d1a9089
Hiroshi Ukai <dakusui@gmail.com>
2025-04-18 04:13:08 +0900

    refactor: Make conductor tool independent of autotest as it can be used manually

--- autotest/src/main/java/com/github/dakusui/simple_tt/tests/Smoke.java
+++ autotest/src/main/java/com/github/dakusui/simple_tt/tests/Smoke.java
@@ -1,7 +1,5 @@
 package com.github.dakusui.simple_tt.tests;
 
-import com.github.dakusui.processstreamer.core.process.ContextualCommandInvoker;
-import com.github.dakusui.processstreamer.launchers.CommandLauncher;
 import com.github.dakusui.processstreamer.launchers.CurlClient;
 import com.github.dakusui.simple_tt.core.Session;
 import com.github.dakusui.simple_tt.core.TestBase;
@@ -13,7 +11,8 @@ import org.junit.jupiter.api.TestInstance;
 
 import static com.github.dakusui.simple_tt.cliches.valid8j.web.Valid8JExpectations.value;
 import static com.github.dakusui.simple_tt.core.TestUtils.navigateToDashboard;
-import static com.github.dakusui.simple_tt.core.TestUtils.navigateToStatus;
+import static com.github.dakusui.simple_tt.core.TestUtils.navigateToHello;
+import static com.github.valid8j.fluent.Expectations.assertStatement;
 import static com.github.valid8j.fluent.Expectations.require;
 import static java.util.stream.Collectors.joining;
 import static jp.co.moneyforward.autotest.actions.web.TableQuery.Term.term;
@@ -24,34 +23,13 @@ import static jp.co.moneyforward.autotest.framework.utils.InsdogUtils.sink;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutotestExecution(defaultExecution = @Spec(
     beforeEach = "screenshot",
-    value = {"toStatus", "toDashboard"},
+    value = {"toHello", "toDashboard", "toDashboard$simplerStyle"},
     afterEach = "screenshot",
     planExecutionWith = DEPENDENCY_BASED))
 public class Smoke extends TestBase {
   @Named
   public Scene nop() {
     return Scene.begin().end();
-  }
-  
-  @DependsOn("openSession")
-  @Named
-  public Scene startServer() {
-    return Scene.begin("session")
-                .add(sink((Session s) -> CommandLauncher.begin()
-                                                        .directory(s.frontendDirectory())
-                                                        .shell(localShell().withProgram("/bin/nohup")
-                                                                           .clearOptions()
-                                                                           .build())
-                                                        .command("npm")
-                                                        .arg("run")
-                                                        .arg("dev")
-                                                        .perform()
-                                                        .forEach(System.err::println)))
-                .end();
-  }
-  
-  private static ContextualCommandInvoker.Builder.ForLocal localShell() {
-    return new ContextualCommandInvoker.Builder.ForLocal();
   }
   
   @Named
@@ -72,39 +50,30 @@ public class Smoke extends TestBase {
   @Named
   @DependsOn("openSession")
   @PreparedBy("nop")
-  @PreparedBy({"killAll", "startServer"})
   public Scene ensureServerIsRunning() {
     return Scene.begin("session")
                 .add(sink((Session s) -> require(value(CurlClient.begin()
                                                                  .option("-s")
                                                                  .option("-w", "%{http_code}")
-                                                                 .option("-o", "/dev/null")
+//                                                                 .option("-o", "/dev/null")
                                                                  .url(s.healthCheckEndpointUrl())
                                                                  .perform()
                                                                  .collect(joining()))
                                                      .toBe()
-                                                     .equalTo("200"))))
+                                                     .endingWith("200"))))
                 .end();
   }
   
-  @Named
-  @DependsOn("ensureServerIsRunning")
-  @PreparedBy("nop")
-  @PreparedBy({"unloadDatasets", "loadDatasets"})
-  public Scene ensureDatasetIsLoaded() {
-    return Scene.begin().end();
-  }
-  
-  @DependsOn({"openSession", "ensureServerIsRunning", "ensureDatasetIsLoaded"})
+  @DependsOn({"openSession", "ensureServerIsRunning"})
   @Export("page")
   @Named
-  public Scene toStatus() {
+  public Scene toHello() {
     return Scene.begin("page")
-                .add(navigateToStatus())
+                .add(navigateToHello())
                 .end();
   }
   
-  @DependsOn({"ensureServerIsRunning", "ensureDatasetIsLoaded", "openSession"})
+  @DependsOn({"ensureServerIsRunning", "openSession"})
   @Export("page")
   @Named
   public Scene toDashboard() {
@@ -114,7 +83,7 @@ public class Smoke extends TestBase {
   }
   
   @Named
-  @When({"toDashboard", "toStatus"})
+  @When({"toDashboard"})
   public Scene thenTestSuiteOfFirstElementInTestSuiteTableIs_First_() {
     return Scene.begin()
                 .assertion((Page r) -> value(r).tableQuery(select("Test Suite")
@@ -126,5 +95,25 @@ public class Smoke extends TestBase {
                                                .toBe()
                                                .containing("First"))
                 .end();
+  }
+  
+  @DependsOn({"ensureServerIsRunning", "openSession"})
+  @Export("page")
+  @Named
+  public void toDashboard$simplerStyle(Page page) {
+    page.navigate("http://localhost:3000/dashboard");
+  }
+  
+  @Named
+  @When({"toDashboard$simplerStyle"})
+  public void thenTestSuiteOfFirstElementInTestSuiteTableIs_First_$simplerStyle(Page r) {
+    assertStatement(value(r).tableQuery(select("Test Suite")
+                                            .from("body table")
+                                            .where(term("#", "0"))
+                                            .$())
+                            .locatorElementAt(0)
+                            .textContent()
+                            .toBe()
+                            .containing("First"));
   }
 }
